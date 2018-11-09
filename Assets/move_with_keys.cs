abb58bbd81a5a1ecc0dfdc9ff0062ca72bf52f84
clem744 <44908545+clem744@users.noreply.github.com>
2018-11-09 23:15:36 +0100

    ajout projet unity

---
+++ Assets/move_with_keys.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_with_keys : MonoBehaviour {
+    public int x = 0;
+    public int y = 0;
+    public int z = 0;
+    public Rigidbody vector;
+    // Use this for initialization
+    void Start () {
+        vector = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+            z = 1;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            z = -1;
+        else
+            z = 0;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            x = 1;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            x = -1;
+        else
+            x = 0;
+        y = -10;
+        vector.velocity = new Vector3(x, y, z);
+    }
+}
