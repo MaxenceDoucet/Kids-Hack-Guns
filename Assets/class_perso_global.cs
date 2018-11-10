1b4269a42971c08c5338e8d707fd304742fd43c5
clem744 <44908545+clem744@users.noreply.github.com>
2018-11-10 08:39:26 +0100

    ajout structure classes perso

---
+++ Assets/class_perso_global.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class class_perso_global : MonoBehaviour {
+    struct perso
+    {
+        class_perso_arbre_competence.perso_arbre_competences arbre;
+        class_perso_stats.perso_stats stats;
+    }
+}
---
+++ Assets/class_perso_stats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class class_perso_stats : MonoBehaviour
+{
+    public struct perso_stats
+    {
+        int degats_percant_distance;
+        int degats_normal_distance;
+        int blindage;
+        int degats_percant_melee;
+        int degats_normal_mele;
+        int nb_blocage;
+        int vieMax;
+        int blessure;
+        int special;
+        int portee;
+        int action_restante;
+        int actionMax;
+    }
+}
