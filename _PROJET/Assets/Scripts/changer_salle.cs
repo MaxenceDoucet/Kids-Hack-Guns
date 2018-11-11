12db9c72c98d2c02b2a26c6e144a595dac58f47d
MaxenceDoucet <doucet.maxence@gmail.com>
2018-11-11 18:43:08 +0100

    Implémentation fond races + brush /!\ ajouter transparence images par graphiste

---
+++ _PROJET/Assets/Scripts/changer_salle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changer_salle : MonoBehaviour {
+	public string Scene_name;
+	public void ChangeScene()
+	{
+		SceneManager.LoadScene(Scene_name);
+	}
+}
